e00747ad02af4f020c53a0fc1020df28df770d91
lmjcbs <hello@liamjacobs.dev>
2019-10-31 22:29:23 +0000

    porject structure

---
+++ lib/top_companies_CLI.rb
@@ -0,0 +1,6 @@
+require "top_companies_CLI/version"
+
+module TopCompaniesCLI
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/top_companies_CLI/version.rb
@@ -0,0 +1,3 @@
+module TopCompaniesCLI
+  VERSION = "0.1.0"
+end
