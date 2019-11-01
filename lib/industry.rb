2ed0102d16a3fd5f7671beaea0d6b2997f0530f6
lmjcbs <hello@liamjacobs.dev>
2019-11-01 16:28:44 +0000

    added class fucntions

--- lib/industry.rb
+++ lib/industry.rb
@@ -1,15 +1,34 @@
 class Industry
 
-  attr_accessor :name
+  attr_accessor :name, :companies
 
   @@all = Array.new
 
   def initialize(industry)
     @name = industry
+    @companies = Array.new
   end
 
   def self.all
     @all
+  end
+
+  def self.save
+    @@all << self
+  end
+
+  def self.create = self.new(name)
+    new_industry = self.new(name)
+    new_industry.save
+    new_industry
+  end
+  
+  def companies
+    @companies
+  end
+
+  def locations
+    self.companies.map { |company| company.location }.uniq
   end
   
 end
