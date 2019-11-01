122ed9744630441da5c6ca93bea18f8956d1267e
lmjcbs <hello@liamjacobs.dev>
2019-11-01 13:47:04 +0000

    cleaned up data values passed to company hashes

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,9 +12,10 @@ class Scraper
       company_hash = {
         name: company.css("a.link.h5").text.strip,
         industry: company.css("div.industry").text.strip,
-        location: company.css("div.location").text.strip,
+        location: company.css("div.location").text.split(',').each(&:strip),
         profile_url: company.css("a.link.h5").attribute("href").value
       }
+      binding.pry
       scraped_companies << company_hash
     end
     scraped_companies
@@ -23,10 +24,10 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     html = Nokogiri::HTML(open(profile_url))
     scraped_company = {
-      employee_satisfaction_percentage: html.css("div.gptw-percentage-label.weight-medium").text,
+      employee_satisfaction_percentage: html.css("div.gptw-percentage-label.weight-medium").text.gsub('%', ''),
       company_bio: html.css("div#about-the-company-content p").text,
       employee_count: html.css("p.small.company-size-number").text.split(" ").first,
-      reviews: html.css("div.text").text
+      reviews: html.css("div.text").text.scan(/[^\.!?]+[\.!?]/).map(&:strip)
     }
     binding.pry
   end
@@ -34,4 +35,4 @@ class Scraper
 end
 
 #Scraper.scrape_profile_page("https://www.greatplacetowork.com/certified-company/1000367")
-#Scraper.scrape_index_page("https://www.greatplacetowork.com/best-workplaces/100-best/2019")
+Scraper.scrape_index_page("https://www.greatplacetowork.com/best-workplaces/100-best/2019")
