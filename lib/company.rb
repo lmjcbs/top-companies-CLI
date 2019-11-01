a51811160ff7335ffa9146d1f0a310852e11f0c3
lmjcbs <hello@liamjacobs.dev>
2019-11-01 18:25:13 +0000

    index page scraper uses profile page scraper

--- lib/company.rb
+++ lib/company.rb
@@ -2,19 +2,18 @@ class Company
 
   extend Concerns::Utilities
 
-  attr_reader :name, :location, :industry, :profile_url, :employee_satisfaction_percentage, :company_bio, :employee_count, :reviews
+  attr_reader :name, :location, :industry, :employee_satisfaction_percentage, :company_bio, :employee_count, :reviews
 
   @@all = Array.new
 
   def initialize(company_hash)
     @name = company_hash[:name]
-    @profile_url = company_hash[:profile_url]
-    @employee_satisfaction_percentage = company_hash[:employee_satisfaction_percentage]
     @company_bio = company_hash[:company_bio]
+    @employee_satisfaction_percentage = company_hash[:employee_satisfaction_percentage]
     @employee_count = company_hash[:employee_count]
     @reviews = company_hash[:reviews]
-    @location = Location.find_or_create_by_name(company_hash[:location])
-    @industry = Industry.find_or_create_by_name(company_hash[:industry])
+    @location = Location.find_or_create_by_name(company_hash[:location]).name
+    @industry = Industry.find_or_create_by_name(company_hash[:industry]).name
   end
 
   def self.create_from_collection(companies_array)
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,21 +8,22 @@ class Scraper
     scraped_companies = Array.new
     html = Nokogiri::HTML(open(index_url))
     html.css("div.col-md-5.col-xs-12.company-text").each do |company|
-      company_hash = {
+      company_index_hash = {
         name: company.css("a.link.h5").text.strip,
         industry: company.css("div.industry").text.strip,
-        location: company.css("div.location").text.split(',').map(&:strip),
-        profile_url: company.css("a.link.h5").attribute("href").value
+        location: company.css("div.location").text.split(',').map(&:strip)[1]
       }
-      
-      scraped_companies << company_hash
+      profile_url = company.css("a.link.h5").attribute("href").value
+      company_profile_hash = self.scrape_profile_page(profile_url)
+      scraped_companies << company_index_hash.merge(company_profile_hash)
     end
+    binding.pry
     scraped_companies
   end
 
   def self.scrape_profile_page(profile_url)
     html = Nokogiri::HTML(open(profile_url))
-    scraped_company = {
+    company_profile_hash = {
       employee_satisfaction_percentage: html.css("div.gptw-percentage-label.weight-medium").text.gsub('%', ''),
       company_bio: html.css("div#about-the-company-content p").text,
       employee_count: html.css("p.small.company-size-number").text.split(" ").first,
@@ -32,5 +33,4 @@ class Scraper
 
 end
 
-#Scraper.scrape_profile_page("https://www.greatplacetowork.com/certified-company/1000367")
-Scraper.scrape_index_page("https://www.greatplacetowork.com/best-workplaces/100-best/2019")
+#Scraper.scrape_index_page("https://www.greatplacetowork.com/best-workplaces/100-best/2019")
