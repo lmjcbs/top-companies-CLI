6ac15ac7675608ff236f2caacca39809131e9176
lmjcbs <hello@liamjacobs.dev>
2019-11-01 15:14:22 +0000

    initialize

--- lib/company.rb
+++ lib/company.rb
@@ -1,6 +1,6 @@
 class Company
 
-  attr_accessor :name, :location, :industry, :profile_url
+  attr_accessor :name, :location, :industry, :profile_url, :employee_satisfaction_percentage, :company_bio, :employee_count, :reviews
 
   @@all = Array.new
 
@@ -8,11 +8,11 @@ class Company
     @name = company_hash[:name]
     @profile_url = company_hash[:profile_url]
     @employee_satisfaction_percentage = company_hash[:employee_satisfaction_percentage]
-    @comapny_bio = company_hash[:comapny_bio]
+    @comapny_bio = company_hash[:company_bio]
     @employee_count = comapny_hash[:employee_count]
     @review = comapny_hash[:reviews]
 
-    #add_object_property(company_hash)
+    #add_object(company_hash)
     #@location =  object
     #@industry =  object
   end
